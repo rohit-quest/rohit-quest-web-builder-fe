790e72f43459043a9b7c64fb377abb91440e209c
soumitra-quest <soumitra.d@questlabs.biz>
2024-11-03 05:44:34 +0530

    design fix in login and generate page

--- src/components/LoginProvider.jsx
+++ src/components/LoginProvider.jsx
@@ -3,7 +3,7 @@ import { ImportConfig } from "../configs/ImportConfig";
 const LoginProvider = ({ children }) => {
   return (
     <div className="flex h-screen w-full bg-gradient-to-b from-gray-900 to-gray-800 text-white">
-      <div className="hidden md:flex flex-col text-center md:w-1/2  w-1/2 justify-center items-center p-12">
+      <div className="hidden md:flex flex-col text-center md:w-1/2  w-1/2 justify-center items-center max-w-[750px] m-auto">
         <div>
           <img
             src={ImportConfig.welcome}
